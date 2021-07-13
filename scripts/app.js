1d87786e5d678a0fe8cb6cb18116a16b4ca2a9fd
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-07-13 15:25:45 -0700

    created timer that now tracks robot's age in seconds

--- scripts/app.js
+++ scripts/app.js
@@ -18,18 +18,36 @@ const friendroid = {
     start(event) {
         console.log('package opened');
         $('.box').addClass('robot-head').removeClass('box');
+        friendroid.startAging();
     },
-    //startAging() {
-    //    $('.age').text(`Age: ${this.age}s old`);
-     //   this.age = setInterval(this., 1000);
-    //}
-}
+    /* 
+        4. create timer - keeps track of age
+        - create method that starts the age timer
+            -- create method aging()
+            --for each second it will add one second to the text in time tag    
+    */
+    startAging() {
+        this.age = setInterval(this.aging, 1000);
+    },
+    aging(){
+        friendroid.age++;
+        console.log(friendroid.age);
+        $('time').text(`Age: ${friendroid.age}s old`);
+
+    },
+};
 
 /* 
     2. event listener on button to begin game
     - select ".open-package" from the dom - done
     - add event listener "click" - done
     - create a start method in friendroid object that the event listener will call -done
+    -
 */
 
 $(".open-package").on("click", friendroid.start);
+/* 
+    5. create meters -> battery drains over time, stimulation fills over time, modifications fills with user input
+    - connect meters to their respective values in friendroid object
+    - 
+*/
